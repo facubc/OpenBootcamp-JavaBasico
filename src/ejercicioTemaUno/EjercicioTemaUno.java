9b675a1a876cf3b88ccf480536fbdc1f6c7ac30e
FacuBc <facundobesse@gmail.com>
2022-08-20 20:56:39 -0300

    ejercicio tema 1

---
+++ src/ejercicioTemaUno/EjercicioTemaUno.java
@@ -0,0 +1,22 @@
+package ejercicioTemaUno;
+
+public class EjercicioTemaUno {
+
+  public static void main(String[] args) {
+    int entero = 34;
+    long enteroLargo = 214748364;
+    double decimal = 3.14;
+    char caracter = 'a';
+    boolean logico = true;
+    String cadena = "cadena de texto";
+
+    System.out.println("int: " + entero);
+    System.out.println("long: " + enteroLargo);
+    System.out.println("decimal: " + decimal);
+    System.out.println("char: " + caracter);
+    System.out.println("boolean: " + logico);
+    System.out.println("String: " + cadena);
+
+  }
+
+}
